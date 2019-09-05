35d69a805af4849661ad1f52b1001afcd8d15fbf
AlekseyGaikevich <alekseygaikevich@gmail.com>
2019-09-05 15:15:58 +0300

    Updates

---
+++ precache-manifest.e45bbe047f1b9f17cc56206b7d73dc88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc04c5b63f65fd1658ee8f054bc54b9",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "8e60dd22d451d31dd20f",
+    "url": "/social-network/static/css/main.ab000467.chunk.css"
+  },
+  {
+    "revision": "341b77872e023c634f13",
+    "url": "/social-network/static/js/2.05c1ceac.chunk.js"
+  },
+  {
+    "revision": "8e60dd22d451d31dd20f",
+    "url": "/social-network/static/js/main.ea4771ed.chunk.js"
+  },
+  {
+    "revision": "cd8926f0a4b82e4e81df",
+    "url": "/social-network/static/js/runtime~main.eb74f080.js"
+  }
+]);
