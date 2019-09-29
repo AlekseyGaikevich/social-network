d1488dfd2050080655942a8a98282238415e73ba
AlekseyGaikevich <alekseygaikevich@gmail.com>
2019-09-29 20:56:41 +0300

    users effect (down)

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -5,20 +5,23 @@ import userPhoto from '../../assets/images/user.png';
 
 const Users = (props) => {
 
-    if (props.users.length === 0) {
+    let getUsers = () => {
+        if (props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(responce => {
-                props.setUsers(responce.data.items);
-            });
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(responce => {
+                    props.setUsers(responce.data.items);
+                });
+        };
     };
 
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={ u.photos.small != null ? u.photos.small : userPhoto } className={styles.userPhoto} alt="" />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt="" />
                     </div>
                     <div>
                         {u.followed
